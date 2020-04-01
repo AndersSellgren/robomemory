5eed3357017d394131c13fd98d77b6b1f9b9adb9
Anders Sellgren <anders.sellgren@gmail.com>
2020-04-01 13:01:53 +0200

    Updates

---
+++ precache-manifest.a30317fa74d527bca253eba18360f214.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec3c25eebfd8d45dc09155fb9d781c79",
+    "url": "/robomemory/index.html"
+  },
+  {
+    "revision": "2ed9435ce89fa3f9429f",
+    "url": "/robomemory/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "42548c1486070537cef3",
+    "url": "/robomemory/static/css/main.d7544699.chunk.css"
+  },
+  {
+    "revision": "2ed9435ce89fa3f9429f",
+    "url": "/robomemory/static/js/2.97eeb36e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robomemory/static/js/2.97eeb36e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42548c1486070537cef3",
+    "url": "/robomemory/static/js/main.fd9aad32.chunk.js"
+  },
+  {
+    "revision": "c5d653f1a909981691d6",
+    "url": "/robomemory/static/js/runtime-main.4059e67f.js"
+  }
+]);
