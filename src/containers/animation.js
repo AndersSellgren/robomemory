0d4b2cec9448500c18690ff65c8ee84b4e47f973
Anders Sellgren <anders.sellgren@gmail.com>
2020-06-18 01:42:17 +0200

    Added some media query and fixed font sizes v2

--- src/containers/animation.js
+++ src/containers/animation.js
@@ -1,4 +1,4 @@
-const animationCards = () => {
+export const animationCards = () => {
   const cardValues = document.querySelectorAll('.card-back')
   setTimeout(() => {
     [...cardValues].map(card => card.classList.add('hideCards'))
@@ -8,13 +8,8 @@ const animationCards = () => {
   },3000)
 }
 
-const victory = () => {
+export const victory = () => {
   const overlayVictory = document.querySelector('.overlay.victory')
   overlayVictory.classList.add('visible')
   // setNewRobots(true)
-}
-
-module.exports = {
-  victory,
-  animationCards
 }
